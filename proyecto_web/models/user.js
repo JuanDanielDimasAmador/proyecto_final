b12a06f46fd213cbe73a1967679ea39de6199fa4
Daniel <flkn059@gmail.com>
2018-07-25 20:26:39 -0500

    añadiendo modelo usuarios

---
+++ proyecto_web/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//User model
+const UserSchema = new Schema({
+    nickname: { type: String, required: true },
+    name: { type: String },
+    facebook_id: { type: String },
+    email: { type: String, required: true },
+    password: { type: String },
+    status: { type: Number, default: 0, required: true },
+    date: { type: Date, default: Date.now(), required: true },
+    city: { type: String }
+
+});
+
+module.exports = User = mongoose.model('users',UserSchema);
