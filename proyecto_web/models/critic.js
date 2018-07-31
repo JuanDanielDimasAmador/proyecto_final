32c22d480ed16f18d8ed24a6c3932a98c70c9357
Daniel <flkn059@gmail.com>
2018-07-31 17:05:48 -0500

    añadiendo modelo de critica

---
+++ proyecto_web/models/critic.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CriticSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: "users" },
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    classification: { type: String, default: "Critica", required: true},
+    likes: [ {
+        user: { type: Schema.Types.ObjectId, ref: "users" }
+    } ],
+    comments: [ {
+        user: { type: Schema.Types.ObjectId, ref: "users" },
+        text: { type: String, required: true },
+        date: { type: Date, default: Date.now() }
+    } ],
+    date: { type: Date, default: Date.now() }
+});
+
+
+module.exports = Critic = mongoose.model("critic",CriticSchema);
