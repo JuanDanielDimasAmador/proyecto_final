bd6dfd999d72d392d9bfd9acdef5fb7ca0623b4f
Daniel <flkn059@gmail.com>
2018-08-02 15:49:12 -0500

    añadiendo clases necesarias al registro

--- proyecto_web/client/src/components/auth/register.js
+++ proyecto_web/client/src/components/auth/register.js
@@ -28,49 +28,51 @@ class Register extends Component {
     render () {
         return (
             <div className="register">
-                <form onSubmit={this.onSubmit} className="register__form">
-                    <div className="register__form--group">
-                        <input type="text"
-                               className="register__form--input"
-                               name="name"
-                               placeholder="Ingrese su nombre"
-                               value={this.state.name}
-                               onChange = {this.onChange}
-                        />
-                        <span className="register__form--input-icon" />
-                    </div>
-                    <div className="register__form--group">
-                        <input type="email"
-                               className="register__form--input"
-                               name="email"
-                               placeholder="Ingrese su correo"
-                               value={this.state.email}
-                               onChange = {this.onChange}
-                        />
-                        <span className="register__form--input-icon" />
-                    </div>
-                    <div className="register__form--group">
-                        <input type="text"
-                               className="register__form--input"
-                               name="password"
-                               placeholder="Ingrese su contraseña"
-                               value={this.state.password}
-                               onChange = {this.onChange}
-                        />
-                        <span className="register__form--input-icon" />
-                    </div>
-                    <div className="register__form--group">
-                        <input type="text"
-                               className="register__form--input"
-                               name="password2"
-                               placeholder="Confirme su contraseña"
-                               value={this.state.password2}
-                               onChange = {this.onChange}
-                        />
-                        <span className="register__form--input-icon" />
-                    </div>
-                    <input type="submit" className="button button-submit"/>
-                </form>
+                <div className="register__container">
+                    <form onSubmit={this.onSubmit} className="register__form">
+                        <div className="register__form--group">
+                            <input type="text"
+                                   className="register__form--input"
+                                   name="name"
+                                   placeholder="Ingrese su nombre"
+                                   value={this.state.name}
+                                   onChange = {this.onChange}
+                            />
+                            <span className="register__form--input-icon" />
+                        </div>
+                        <div className="register__form--group">
+                            <input type="email"
+                                   className="register__form--input"
+                                   name="email"
+                                   placeholder="Ingrese su correo"
+                                   value={this.state.email}
+                                   onChange = {this.onChange}
+                            />
+                            <span className="register__form--input-icon" />
+                        </div>
+                        <div className="register__form--group">
+                            <input type="text"
+                                   className="register__form--input"
+                                   name="password"
+                                   placeholder="Ingrese su contraseña"
+                                   value={this.state.password}
+                                   onChange = {this.onChange}
+                            />
+                            <span className="register__form--input-icon" />
+                        </div>
+                        <div className="register__form--group">
+                            <input type="text"
+                                   className="register__form--input"
+                                   name="password2"
+                                   placeholder="Confirme su contraseña"
+                                   value={this.state.password2}
+                                   onChange = {this.onChange}
+                            />
+                            <span className="register__form--input-icon" />
+                        </div>
+                        <input type="submit" className="button button-submit"/>
+                    </form>
+                </div>
             </div>
         );
     }
