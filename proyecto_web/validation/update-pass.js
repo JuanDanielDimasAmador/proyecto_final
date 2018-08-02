def8029e748dba7208d1dd5ae362ec2cba44c08c
Daniel <flkn059@gmail.com>
2018-08-02 15:18:05 -0500

    añadir validacion al actualizar contraseña

--- proyecto_web/validation/update-pass.js
+++ proyecto_web/validation/update-pass.js
@@ -7,12 +7,13 @@ module.exports = validateUpdatePassInput = (data) => {
     data.password = !isEmpty(data.password) ? data.password : "" ;
     data.password2 = !isEmpty(data.password2) ? data.password2 : "" ;
 
+    if (Validator.equals(data.password, data.password2)) errors.password2 = "La nueva contraseña no debe ser igual a la antigua";
+
     if (Validator.isEmpty(data.password)) errors.password = "Campo requerido";
 
     if (Validator.isEmpty(data.password2)) errors.password2 = "Campo requerido";
 
     if (!Validator.isLength(data.password2, {min: 6, max: 30})) errors.password2 = "la contraseña debe de contener entre 6 y 30 caracteres";
 
-    if (Validator.equals(data.password, data.password2))  errors.password2  = "La nueva contraseña no debe ser igual a la antigua";
 
 };
